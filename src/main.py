3a5a9bed786aea71567bd28ec6ab00d20f9910f4
Mik Kröis <mik.pedersen@ufst.dk>
2022-06-01 23:53:08 +0200

    now able to download youtube video

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+import os
+import sys
+from ytdl import ytdl
+
+
 def main():
-    print("Hello World!")
+    sourceOfDownload = sys.argv[1:]
+    #os.system("screen -q")
+    #print("Using screen!")
+    #print("you can detach screen by ctrl+d")
+    #print("for at list of screens type screen -ls")
+
+    #os.chdir("/home/hdd/sambashare/music")
+    os.chdir("/Users/krois/Source/ytdl")
+    print(sourceOfDownload)
+    print("use screen -wipe to close all screen sessions")
+
+    ytdl.doDownload(sourceOfDownload)
 
 if __name__ == "__main__":
     main()
---
+++ src/ytdl.py
@@ -0,0 +1,16 @@
+from distutils.log import info
+from yt_dlp import YoutubeDL
+
+class ytdl:
+    def doDownload(sourceToDownload):
+        ydl_opts = {
+                'outtmpl': '%(title)s.%(ext)s',
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]}
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download(sourceToDownload)
+
